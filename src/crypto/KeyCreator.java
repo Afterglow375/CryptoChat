0152bfe51888f8d474d81ca481a5001313acbc4d
Afterglow375 <alex.tatusko@gmail.com>
2014-04-17 01:49:59 -0400

    Keywords create a SecretKeySpec

--- src/crypto/KeyCreator.java
+++ src/crypto/KeyCreator.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import javax.crypto.spec.SecretKeySpec;
 
 import android.content.Context;
-import android.widget.TextView;
 
 import com.example.crypto_app.R;
 
@@ -21,6 +20,8 @@ public class KeyCreator {
 	private static KeyCreator instance = null;
 	private ArrayList<String> words = new ArrayList<String>(DICT_SIZE);
 	private ArrayList<String> keywords = new ArrayList<String>(5);
+	private ArrayList<Integer> createdKeywordIndices = new ArrayList<Integer>(5);
+	private ArrayList<Integer> enteredKeywordIndices = new ArrayList<Integer>(5);
 	private SecureRandom sr = new SecureRandom();
 	
 	// Load the dictionary from memory into an ArrayList
@@ -51,23 +52,50 @@ public class KeyCreator {
 	// PRNG used to pick 5 random keywords from the dictionary
 	public ArrayList<String> generateWords() {
 		keywords.clear();
-		keywords.add(words.get(sr.nextInt(DICT_SIZE-1)));
-		keywords.add(words.get(sr.nextInt(DICT_SIZE-1)));
-		keywords.add(words.get(sr.nextInt(DICT_SIZE-1)));
-		keywords.add(words.get(sr.nextInt(DICT_SIZE-1)));
-		keywords.add(words.get(sr.nextInt(DICT_SIZE-1)));
+		createdKeywordIndices.clear();
+		for (int i = 0; i < 5; i++) {
+			int wordIndex = sr.nextInt(DICT_SIZE-1);
+			keywords.add(words.get(wordIndex));
+			createdKeywordIndices.add(wordIndex);
+		}
 		return keywords;
 	}
 	
 	// Creates a key from 10 keywords
 	public SecretKeySpec createKey() {
-		AddContactActivity.getEnteredKeywords();
-		BigInteger key = BigInteger.valueOf(1);
-		ArrayList<Integer> keywordIndices = new ArrayList<Integer>(10);
 		keywords = AddContactActivity.getEnteredKeywords();
-		for (int i = 9; i>=0; i--) {
-			key = key.multiply(BigInteger.valueOf(keywordIndices.get(i)));
+		ArrayList<Integer> organizedKeywordIndices = organizeKeywordIndices();
+		
+		// Compute the key, k = k*n + a
+		BigInteger key = BigInteger.valueOf(0);
+		for (int i = 0; i<10; i++) {
+			key = key.multiply(BigInteger.valueOf(organizedKeywordIndices.get(i))).add(BigInteger.valueOf(createdKeywordIndices.get(i)));
 		}
 		return new SecretKeySpec(key.toByteArray(), "AES");
 	}
+	
+	private ArrayList<Integer> organizeKeywordIndices() {
+		enteredKeywordIndices.clear();
+		for (int i = 0; i < 5; i++) {
+			String keyword = keywords.get(i).toLowerCase().trim();
+			enteredKeywordIndices.add(words.indexOf(keyword));
+		}
+	
+		ArrayList<Integer> organizedKeywordIndices = new ArrayList<Integer>(10);
+		for (int i = 0; i < 5; i++) {
+			if (createdKeywordIndices.get(i) < enteredKeywordIndices.get(i)) {
+				organizedKeywordIndices.addAll(createdKeywordIndices);
+				organizedKeywordIndices.addAll(enteredKeywordIndices);
+				return organizedKeywordIndices;
+			}
+			else if (createdKeywordIndices.get(i) > enteredKeywordIndices.get(i)) {
+				organizedKeywordIndices.addAll(createdKeywordIndices);
+				organizedKeywordIndices.addAll(enteredKeywordIndices);
+				return organizedKeywordIndices;
+			}
+		}
+		organizedKeywordIndices.addAll(createdKeywordIndices);
+		organizedKeywordIndices.addAll(enteredKeywordIndices);
+		return organizedKeywordIndices;
+	}		
 }
